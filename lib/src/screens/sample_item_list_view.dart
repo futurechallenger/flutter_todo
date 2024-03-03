06798da426004fb6f241c58a4863717b23cc827a
bruce.li <unclecharlie@Uncles-MacBook-Pro.local>
2024-03-04 00:25:08 +0800

    refactor list view with future-builder

--- lib/src/screens/sample_item_list_view.dart
+++ lib/src/screens/sample_item_list_view.dart
@@ -1,42 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/src/common/http_request.dart';
+import 'package:flutter_todo/src/models/todo_model.dart';
 
-import 'sample_item.dart';
 import 'sample_item_details_view.dart';
 
 /// Displays a list of SampleItems.
-class SampleItemListView extends StatelessWidget {
-  const SampleItemListView({super.key, required this.items});
+class SampleItemListView extends StatefulWidget {
+  const SampleItemListView({super.key});
 
   static const routeName = '/';
-  final List<SampleItem> items;
+
+  @override
+  State<SampleItemListView> createState() => _SampleItemListViewState();
+}
+
+class _SampleItemListViewState extends State<SampleItemListView> {
+  late Future<List<TodoItem>?> items;
+
+  @override
+  void initState() {
+    super.initState();
+
+    items = HttpRequest().fetchTodoList('uncompleted');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      // Providing a restorationId allows the ListView to restore the
-      // scroll position when a user leaves and returns to the app after it
-      // has been killed while running in the background.
-      restorationId: 'sampleItemListView',
-      itemCount: items.length,
-      itemBuilder: (BuildContext context, int index) {
-        final item = items[index];
+    return FutureBuilder<List<TodoItem>?>(
+        future: items,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              // Providing a restorationId allows the ListView to restore the
+              // scroll position when a user leaves and returns to the app after it
+              // has been killed while running in the background.
+              restorationId: 'sampleItemListView',
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                final item = items[index];
 
-        return ListTile(
-            title: Text('SampleItem ${item.id}'),
-            leading: const CircleAvatar(
-              // Display the Flutter Logo image asset.
-              foregroundImage: AssetImage('assets/images/flutter_logo.png'),
-            ),
-            onTap: () {
-              // Navigate to the details page. If the user leaves and returns to
-              // the app after it has been killed while running in the
-              // background, the navigation stack is restored.
-              Navigator.restorablePushNamed(
-                context,
-                SampleItemDetailsView.routeName,
-              );
-            });
-      },
-    );
+                return ListTile(
+                    title: Text('SampleItem ${item.id}'),
+                    leading: const CircleAvatar(
+                      // Display the Flutter Logo image asset.
+                      foregroundImage:
+                          AssetImage('assets/images/flutter_logo.png'),
+                    ),
+                    onTap: () {
+                      // Navigate to the details page. If the user leaves and returns to
+                      // the app after it has been killed while running in the
+                      // background, the navigation stack is restored.
+                      Navigator.restorablePushNamed(
+                        context,
+                        SampleItemDetailsView.routeName,
+                      );
+                    });
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          return const CircularProgressIndicator();
+        });
   }
 }
