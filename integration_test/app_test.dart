32b11ca1630ee27e09148334b4884a54d2c27d01
Uncle Charlie <sharp.bruce.lee@gmail.com>
2024-03-29 16:02:23 +0800

    run test in ios simulator

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_todo/main.dart';
@@ -9,22 +10,12 @@ void main() {
 
   group('end-to-end test', () {
     testWidgets('hello world test', (tester) async {
-      // final client = MockClient();
-      // final httpRequest = hs.HttpRequest();
-      // httpRequest.client = client;
-
-      // when(client.get(Uri.parse('http://localhost:17788/list/all'))).thenAnswer(
-      //     (_) async => http.Response(
-      //         '{"data": [{"id":1, "content": "hello","note": "&", "status": 0, "deleted": 0}], "message": "ok"}',
-      //         200));
-
       await tester.pumpWidget(const GetxApplication());
 
-      debugDumpApp();
-
+      await tester.pumpAndSettle();
       final listTileFinder = find.text("hello");
 
-      expect(listTileFinder, findsOneWidget);
+      expect(listTileFinder, findsAtLeast(1));
     });
   });
 }
