6d89c2075a66e8158ebf860f23b2604086a4bf30
Uncle Charlie <sharp.bruce.lee@gmail.com>
2024-06-10 18:00:15 +0800

    remove no use tween animation

--- lib/src/getx/pages/animations/TweenAnimationPage.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class TweenAnimationPage extends StatelessWidget {
-  
-  @override
-  Widget build(BuildContext context) {
-    return const Column(children: [Flexible(child: Text("Tween Animation"))],);
-  }
-}
