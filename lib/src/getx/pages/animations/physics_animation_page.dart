70ea1b5ffa43f1c1888d871d97fc7aa8eeb2070c
Uncle Charlie <sharp.bruce.lee@gmail.com>
2024-06-13 20:12:31 +0800

    physics animation -- spring animation

--- lib/src/getx/pages/animations/physics_animation_page.dart
+++ lib/src/getx/pages/animations/physics_animation_page.dart
@@ -11,70 +11,70 @@ class PhysicsAnimationPage extends StatefulWidget {
 class _PhysicsAnimationPageState extends State<PhysicsAnimationPage>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _animation;
+  // late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
 
-    _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2))
-          ..addListener(() {
-            setState(() {});
-          });
+    _controller = AnimationController(vsync: this, upperBound: 500)
+      ..addListener(() {
+        setState(() {});
+      });
 
-    _animation = Tween<double>(begin: 0, end: 400).animate(
-        CurvedAnimation(parent: _controller, curve: Curves.elasticOut));
+    // _animation = Tween<double>(begin: 0, end: 400).animate(
+    //     CurvedAnimation(parent: _controller, curve: Curves.elasticOut));
 
-    _controller.forward();
+    final simulation = SpringSimulation(
+        const SpringDescription(mass: 1, stiffness: 10, damping: 1),
+        0,
+        300,
+        10);
+
+    // _controller.forward();
+    _controller.animateWith(simulation);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Physics Animation"),
-      ),
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _animation,
-          builder: (context, child) {
-            return Stack(
-              children: [
-                Positioned(
-                  left: 70,
-                  top: _animation.value,
-                  child: child!,
-                )
-              ],
-            );
-          },
-          child: GestureDetector(
-            onTap: () {
-              _startAnimation();
-            },
-            child: Container(
-              height: 100,
-              width: 250,
-              decoration: BoxDecoration(
-                color: const Color(0xFF00EF3C),
-                borderRadius: BorderRadius.circular(10),
+        appBar: AppBar(
+          title: const Text("Physics Animation"),
+        ),
+        body: Stack(children: [
+          Positioned(
+            left: 50,
+            top: _controller.value,
+            height: 40,
+            width: 40,
+            child: GestureDetector(
+              // onPanEnd: (details) {
+              //   _startAnimation(details.velocity.pixelsPerSecond.dx);
+              // },
+              onTap: () {
+                // _startAnimation(10);
+              },
+              child: Container(
+                color: Colors.blue,
               ),
-              child: const Center(
-                  child: Text(
-                'SEMAPHORE',
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              )),
             ),
           ),
-        ),
-      ),
-    );
+        ]));
   }
 
-  void _startAnimation() {
-    _controller.reset();
-    _controller.forward();
+  void _startAnimation(double velocity) {
+    final simulation = SpringSimulation(
+      const SpringDescription(
+        mass: 1,
+        stiffness: 100,
+        damping: 1,
+      ),
+      _controller.value,
+      500, // end position
+      20,
+    );
+
+    _controller.animateWith(simulation);
   }
 
   @override
