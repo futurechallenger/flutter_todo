103d2a688f5b2603c256881a5a5846b55d0bbf13
Uncle Charlie <sharp.bruce.lee@gmail.com>
2024-06-23 22:45:20 +0800

    animated-container

--- lib/src/getx/pages/animations/implicit_animation_page.dart
+++ lib/src/getx/pages/animations/implicit_animation_page.dart
@@ -1,4 +1,19 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+double randomBorderRadius() {
+  return Random().nextDouble() * 64;
+}
+
+double randomMargin() {
+  return Random().nextDouble() * 64;
+}
+
+Color randomColor() {
+  return Color(0xFFFFFFFF & Random().nextInt(0xFFFFFFFF));
+}
 
 class ImplicitAnimationPage extends StatefulWidget {
   const ImplicitAnimationPage({super.key});
@@ -8,14 +23,70 @@ class ImplicitAnimationPage extends StatefulWidget {
 }
 
 class _ImplicitAnimationPageState extends State<ImplicitAnimationPage> {
+  double opacity = 0;
+  //
+  Color color = Colors.white;
+  double radius = 0;
+  double margin = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    color = randomColor();
+    radius = randomBorderRadius();
+    margin = randomMargin();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Implicit Animation"),
+        actions: [
+          TextButton(
+              onPressed: () {
+                setState(() {
+                  opacity = 1;
+                });
+              },
+              child: const Text("Start 1")),
+          TextButton(
+              onPressed: () {
+                setState(() {
+                  color = randomColor();
+                  radius = randomBorderRadius();
+                  margin = randomMargin();
+                });
+              },
+              child: const Text("Start 2"))
+        ],
       ),
       body: Container(
-        child: const Text("Implicit Animation"),
+        color: Colors.yellow,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: AnimatedOpacity(
+                duration: const Duration(seconds: 2),
+                opacity: opacity,
+                child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 500),
+                    margin: EdgeInsets.all(margin),
+                    decoration: BoxDecoration(
+                        color: color,
+                        borderRadius: BorderRadius.circular(radius)),
+                    child: const Text(
+                      "Implicit Animation",
+                      style: TextStyle(fontSize: 30),
+                    )),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
