2bb136f732a3aad17f433ee699f5ef79ba796d74
uncle charlie <uncle_charlie@maddog.com>
2024-08-16 16:29:38 +0800

    add texture view in a page

--- lib/src/common/native_texture_layer_view.dart
+++ lib/src/common/native_texture_layer_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NativeTextureLayerView extends StatelessWidget {
   const NativeTextureLayerView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const String viewType = "@views/native_example_view";
+    final Map<String, dynamic> creationParams = <String, dynamic>{};
+
+    return AndroidView(
+        viewType: viewType,
+        layoutDirection: TextDirection.ltr,
+        creationParams: creationParams,
+        creationParamsCodec: const StandardMessageCodec());
   }
 }
--- lib/src/getx/texture_layer_native_page.dart
+++ lib/src/getx/texture_layer_native_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/src/common/native_texture_layer_view.dart';
 
 class TextureLayerNativePage extends StatelessWidget {
   const TextureLayerNativePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Texture Layer"),
+      ),
+      body: const Center(child: NativeTextureLayerView()),
+    );
   }
 }
