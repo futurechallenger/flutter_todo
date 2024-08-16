52c4bc954c215ba8c76092661d05e08882641a8b
uncle charlie <uncle_charlie@maddog.com>
2024-08-16 11:38:05 +0800

    platform view placeholder widget

---
+++ lib/src/common/native_example_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NativeExampleView extends StatelessWidget {
+  const NativeExampleView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Native view here");
+  }
+}
