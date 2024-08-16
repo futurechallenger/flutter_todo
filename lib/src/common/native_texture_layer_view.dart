3e5aa3ab79ee669dcc272f1a2cdd30e1dc0ce2e5
uncle charlie <uncle_charlie@maddog.com>
2024-08-16 16:12:24 +0800

    texture layer native android

---
+++ lib/src/common/native_texture_layer_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NativeTextureLayerView extends StatelessWidget {
+  const NativeTextureLayerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
